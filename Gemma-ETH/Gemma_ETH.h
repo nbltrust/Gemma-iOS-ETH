06b7f94bb4b09807f3aed58885c103968addadde
koofrank <koofranker@gmail.com>
2018-10-10 12:11:14 +0800

    init project

---
+++ Gemma-ETH/Gemma_ETH.h
@@ -0,0 +1,19 @@
+//
+//  Gemma_ETH.h
+//  Gemma-ETH
+//
+//  Created by koofrank on 2018/10/10.
+//  Copyright © 2018 com.nbltrustdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gemma_ETH.
+FOUNDATION_EXPORT double Gemma_ETHVersionNumber;
+
+//! Project version string for Gemma_ETH.
+FOUNDATION_EXPORT const unsigned char Gemma_ETHVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gemma_ETH/PublicHeader.h>
+
+
